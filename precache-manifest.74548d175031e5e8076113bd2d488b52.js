5c0d78a6b0e59d642e9d96a177b14daeb1f48d34
andyh2 <andy@comma.ai>
2020-02-19 12:40:45 -0500

    Updates

---
+++ precache-manifest.74548d175031e5e8076113bd2d488b52.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e445363ca8f61f8213e6512ac7d68f8",
+    "url": "/d8ec75342437d86094b0.worker.js"
+  },
+  {
+    "revision": "3bebee72173dd7d089d8e492a2174690",
+    "url": "/index.html"
+  },
+  {
+    "revision": "434801cd818addbf0535",
+    "url": "/static/css/2.598d3478.chunk.css"
+  },
+  {
+    "revision": "88306e01563c97a4da15",
+    "url": "/static/css/main.a16d780c.chunk.css"
+  },
+  {
+    "revision": "434801cd818addbf0535",
+    "url": "/static/js/2.b55d4150.chunk.js"
+  },
+  {
+    "revision": "88306e01563c97a4da15",
+    "url": "/static/js/main.75750c85.chunk.js"
+  },
+  {
+    "revision": "6e9ba062b08c9d8eb9c5",
+    "url": "/static/js/runtime-main.9e2219b8.js"
+  },
+  {
+    "revision": "368fbfd61326403ee20f91629f2af514",
+    "url": "/static/media/icon-chffr-wheel.368fbfd6.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cfa3dbfb8f5008a828cb9999db0debf8.js"
+  "/precache-manifest.74548d175031e5e8076113bd2d488b52.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
